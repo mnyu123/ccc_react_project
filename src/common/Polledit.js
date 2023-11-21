e585e1cd3fbe68815f50d4e16cd1ffe5b8c51c82
mnyu123 <whdudwo5582@gmail.com>
2023-11-21 15:05:57 +0900

    설문 수정 페이지 리액트 추가

---
+++ src/common/Polledit.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import '../css/Polledit.css';
+
+const Polledit = (props) => {
+    const [showPopup, setShowPopup] = useState(true);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const genres = [
+        ['소설', '시/에세이', '인문', '가정/육아', '요리', '건강', '취미/실용', '경제', '자기계발'],
+        ['정치/사회', '역사/문화', '종교', '예술', '사회과학', '자연과학', '공학', '여행', '컴퓨터과학'],
+        ['참고서', '잡지', '취업/수험서', '청소년', '만화']
+    ];
+
+    const submitForm = (e) => {
+        e.preventDefault();
+        const checkboxes = document.querySelectorAll('input[type=checkbox]:checked');
+        if (checkboxes.length === 0) {
+            setErrorMessage('적어도 하나의 장르를 선택해 주세요.');
+        } else {
+            setErrorMessage('');
+            setShowPopup(false);
+        }
+    }
+
+    return (
+        <div id="popup" className={`popup ${showPopup ? "show-popup" : ""}`}>
+            <div className="overlay"></div>
+            <div className="content-title">
+                <div className="title-container">
+                    <h1>선호 장르 조사</h1>
+                    <button className="close-btn" onClick={props.onClose}>
+                        <img src="close.png" alt="close" />
+                    </button>
+                </div>
+            </div>
+            <div className="content-explain">
+                <h2>책 선호 장르를 선택해 주세요.</h2>
+            </div>
+            <div className="content">
+                {genres.map((genreGroup, index) => (
+                    <form id={`genre-form${index + 1}`}>
+                        {genreGroup.map(genre => (
+                            <>
+                                <input type='checkbox' name='genre' value={genre} /> {genre}<br />
+                            </>
+                        ))}
+                    </form>
+                ))}
+                <div className='positioned-container'>
+                    <p className="error-message" style={{ display: errorMessage ? 'block' : 'none' }}>{errorMessage}</p>
+                    <button className="signup-btn" onClick={submitForm}>수정</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Polledit;
--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,37 +1,47 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "../css/Mypage.css"; // Mypage.css를 불러옵니다.
+import Polledit from "../common/Polledit"; // 수정된 부분
 
-class Mypage extends Component {
-  render() {
-    return (
-      <div className={`modal ${this.props.isMypageOpen ? "open" : ""}`}>
-        <div className="modal-content">
-          <h2>마이페이지</h2>
-          <p>비밀번호 재확인</p>
-          <p>보안을 위해 비밀번호를 다시 입력해주세요.</p>
-          <div className="myname">
-            <span>
-              <img src="/images/ccc_image/ghost.png" alt="profile" />
-            </span>
-            <span className="username_wrap">
-              <input
-                type="text"
-                value="사용자"
-                className="username"
-                id="username"
-                readOnly
-              />
-            </span>
-            <span>님,</span>
-          </div>
-          <input type="password" placeholder="비밀번호" />
-          <button>확인</button>
-          <button>로그아웃</button>
-          <button onClick={this.props.onClose}>닫기</button>
+const Mypage = ({ isMypageOpen, onClose }) => {
+  const [isPolleditOpen, setIsPolleditOpen] = useState(false);
+
+  const openPolledit = () => {
+    setIsPolleditOpen(true);
+  };
+
+  const closePolledit = () => {
+    setIsPolleditOpen(false);
+  };
+  return (
+    <div className={`modal ${isMypageOpen ? "open" : ""}`}>
+      <div className="modal-content">
+        <h2>마이페이지</h2>
+        <p>비밀번호 재확인</p>
+        <p>보안을 위해 비밀번호를 다시 입력해주세요.</p>
+        <div className="myname">
+          <span>
+            <img src="/images/ccc_image/ghost.png" alt="profile" />
+          </span>
+          <span className="username_wrap">
+            <input
+              type="text"
+              value="사용자"
+              className="username"
+              id="username"
+              readOnly
+            />
+          </span>
+          <span>님,</span>
         </div>
+        <input type="password" placeholder="비밀번호" />
+        <button>확인</button>
+        <button>로그아웃</button>
+        <button onClick={openPolledit}>장르 재선택</button>
+        <button onClick={onClose}>닫기</button>
+        {isPolleditOpen && <Polledit onClose={closePolledit} />}  // 수정된 부분
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Mypage;
