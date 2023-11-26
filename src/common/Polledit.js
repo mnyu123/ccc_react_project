c0303340242fb31049b5f014909a21660a26c000
Torku7 <saturn9908>
2023-11-26 16:03:58 +0900

    장르변경 적용중

--- src/common/Polledit.js
+++ src/common/Polledit.js
@@ -1,48 +1,75 @@
 import React, { useState } from "react";
 import "../css/Polledit.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const genres = [
+  "소설",
+  "시/에세이",
+  "인문",
+  "가정/육아",
+  "요리",
+  "건강",
+  "취미/실용",
+  "경제",
+  "자기계발",
+  "정치/사회",
+  "역사/문화",
+  "종교",
+  "예술",
+  "사회과학",
+  "자연과학",
+  "공학",
+  "여행",
+  "컴퓨터과학",
+  "참고서",
+  "잡지",
+  "취업/수험서",
+  "청소년",
+  "만화",
+];
 
 const Polledit = (props) => {
+  const [selectedGenres, setSelectedGenres] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(false);
   const [showPopup, setShowPopup] = useState(true);
-  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+  const userid = JSON.parse(sessionStorage.getItem('userid'));
 
-  const genres = [
-    [
-      "소설",
-      "시/에세이",
-      "인문",
-      "가정/육아",
-      "요리",
-      "건강",
-      "취미/실용",
-      "경제",
-      "자기계발",
-    ],
-    [
-      "정치/사회",
-      "역사/문화",
-      "종교",
-      "예술",
-      "사회과학",
-      "자연과학",
-      "공학",
-      "여행",
-      "컴퓨터과학",
-    ],
-    ["참고서", "잡지", "취업/수험서", "청소년", "만화"],
-  ];
-
-  const submitForm = (e) => {
-    e.preventDefault();
-    const checkboxes = document.querySelectorAll(
-      "input[type=checkbox]:checked"
-    );
-    if (checkboxes.length === 0) {
-      setErrorMessage("적어도 하나의 장르를 선택해 주세요.");
+  const handleCheck = (genre) => {
+    if (selectedGenres.includes(genre)) {
+      setSelectedGenres(selectedGenres.filter((g) => g !== genre));
     } else {
-      setErrorMessage("");
-      setShowPopup(false);
+      setSelectedGenres([...selectedGenres, genre]);
     }
   };
+
+ const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (selectedGenres.length === 0) {
+      setErrorMessage(true);
+    } else {
+      try {
+        const user = {
+          userid, // 사용자 아이디를 요청 본문에 추가
+          usergenre: {
+            genre1: selectedGenres[0],
+            genre2: selectedGenres[1],
+            genre3: selectedGenres[2],
+          },
+        };
+        const response = await axios.post("http://localhost:3000/polledit", user);
+        console.log(response.data);
+        setErrorMessage(false);
+        setShowPopup(false);
+        navigate("/");
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+
 
   return (
     <div id="popup" className={`popup ${showPopup ? "show-popup" : ""}`}>
@@ -59,24 +86,21 @@ const Polledit = (props) => {
         <h2>책 선호 장르를 선택해 주세요.</h2>
       </div>
       <div className="content">
-        {genres.map((genreGroup, index) => (
-          <form id={`genre-form${index + 1}`}>
-            {genreGroup.map((genre) => (
-              <>
-                <input type="checkbox" name="genre" value={genre} /> {genre}
-                <br />
-              </>
-            ))}
-          </form>
+        {genres.map((genre, index) => (
+          <div key={index}>
+            <input
+              type="checkbox"
+              name="genre"
+              value={genre}
+              checked={selectedGenres.includes(genre)}
+              onChange={() => handleCheck(genre)}
+            />{" "}
+            {genre}
+          </div>
         ))}
         <div className="positioned-container">
-          <p
-            className="error-message"
-            style={{ display: errorMessage ? "block" : "none" }}
-          >
-            {errorMessage}
-          </p>
-          <button className="signup-btn" onClick={submitForm}>
+          {errorMessage && <p className="error-message">적어도 하나의 장르를 선택해 주세요.</p>}
+          <button className="signup-btn" onClick={handleSubmit}>
             수정
           </button>
         </div>
