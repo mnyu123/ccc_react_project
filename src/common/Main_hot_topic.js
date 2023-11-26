d961aa835a446381a5b8cff60c575695a304334a
mnyu123 <whdudwo5582@gmail.com>
2023-11-26 14:55:39 +0900

    css 수정 전 API 책 정보 조회 메인페이지 출력

--- src/common/Main_hot_topic.js
+++ src/common/Main_hot_topic.js
@@ -1,119 +1,38 @@
 import React, { useState, useEffect } from "react";
 import "../css/Main_hot_topic.css";
 
+import axios from "axios";
+
 const MainHotTopic = () => {
-  // 임시 데이터
-  const slidesData = [
-    {
-      img: "/images/ccc_bookcover/도쿄 에일리언즈 7.jpg",
-      alt: "책 표지",
-      number: 1,
-      description: "설명",
-    },
-    {
-      img: "/images/ccc_bookcover/황제의 외동딸(만화) 1.jpg",
-      alt: "책 표지",
-      number: 2,
-      description: "설명",
-    },
-    {
-      img: "/images/ccc_bookcover/하루만 네가 되고 싶어.jpg",
-      alt: "책 표지",
-      number: 3,
-      description: "설명",
-    },
-    {
-      img: "/images/ccc_bookcover/주술회전.jpg",
-      alt: "책 표지",
-      number: 4,
-      description: "설명",
-    },
-    {
-      img: "/images/ccc_bookcover/오투 중등 과학 3-2(2023).jpg",
-      alt: "책 표지",
-      number: 5,
-      description: "설명",
-    },
-    {
-      img: "/images/ccc_bookcover/역대급 영지 설계사 1.jpg",
-      alt: "책 표지",
-      number: 6,
-      description: "설명",
-    },
-    {
-      img: "/images/ccc_bookcover/명탐정코난.jpg",
-      alt: "책 표지",
-      number: 7,
-      description: "설명",
-    },
-    {
-      img: "/images/ccc_bookcover/쎈 중등 수학 2-2(2023).jpg",
-      alt: "책 표지",
-      number: 8,
-      description: "설명",
-    },
-    {
-      img: "/images/ccc_bookcover/아레나 옴므+(2023년 11월호).jpg",
-      alt: "책 표지",
-      number: 9,
-      description: "설명",
-    },
-  ];
-
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const slides = slidesData;
-
-  const updateSlidePosition = () => {
-    const slideContainer = document.querySelector(".slide");
-    slideContainer.style.transform = `translateX(-${
-      currentIndex * (100 / slides.length)
-    }%)`;
-  };
+  // api 데이터
+  const [books, setBooks] = useState([]);
 
   useEffect(() => {
-    updateSlidePosition();
-  }, [currentIndex]);
-
-  const prevSlide = () => {
-    setCurrentIndex((prevIndex) => {
-      let newIndex = prevIndex - 1;
-      if (newIndex < 0) newIndex = slides.length - 1;
-      console.log("이전 버튼 눌려짐"); // 이 부분 추가
-      return newIndex;
-    });
-  };
-
-  const nextSlide = () => {
-    setCurrentIndex((prevIndex) => {
-      let newIndex = prevIndex + 1;
-      if (newIndex >= slides.length) newIndex = 0;
-      console.log("다음 버튼 눌려짐"); // 이 부분 추가
-      return newIndex;
-    });
-  };
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`/api/aladin/해리포터`);
+        setBooks(response.data.item);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <div className="soaring_wrap" id="soaring_wrap">
       <h2>급상승</h2>
       <div className="slide">
         <div className="soaring1">
-          {slides.map((slide, index) => (
-            <div key={index} className="item">
-              <img src={slide.img} alt={slide.alt} />
-              <span className="number">{slide.number}</span>
-              <p className="description">{slide.description}</p>
+          {books.map((book, index) => (
+            <div key={book.isbn} className="item">
+              <img src={book.cover} alt={book.title} />
+              <span className="number">{index + 1}</span>
+              <p className="description">{book.description}</p>
             </div>
           ))}
         </div>
-        <button id="nextBtn" onClick={nextSlide}>
-          {" "}
-          {">"}{" "}
-        </button>
       </div>
-      <button id="prevBtn" onClick={prevSlide}>
-        {" "}
-        {"<"}{" "}
-      </button>
     </div>
   );
 };
--- src/common/Main_monthbook.js
+++ src/common/Main_monthbook.js
@@ -1,51 +1,21 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "../css/Main_monthbook.css";
 
 const Main_monthbook = () => {
-  const [books, setBooks] = useState([
-    { title: "돈의 심리학", author: "Author1" },
-    { title: "빠르게 실패하기", author: "Author2" },
-    { title: "세탁 살림 백과", author: "Author3" },
-    { title: "적당히 살기", author: "Author4" },
-    { title: "돈의 역사", author: "Author5" },
-    // 여기에 메인에 보일 책을 더 추가
-  ]);
+  const [books, setBooks] = useState([]);
   const [viewMore, setViewMore] = useState(false); // 더보기 상태
 
-  // 책 표지 이미지를 무작위로 섞는 함수
-  const shuffle = (array) => {
-    let currentIndex = array.length,
-      temporaryValue,
-      randomIndex;
-
-    while (0 !== currentIndex) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-  };
-
   useEffect(() => {
-    const bookImages = [
-      "/images/ccc_other/돈의 심리학.webp",
-      "/images/ccc_other/빠르게 실패하기.webp",
-      "/images/ccc_other/세탁 살림 백과.webp",
-      "/images/ccc_other/돈의 심리학.webp",
-      "/images/ccc_other/빠르게 실패하기.webp",
-      "/images/ccc_other/세탁 살림 백과.webp",
-      "/images/ccc_other/돈의 심리학.webp",
-      "/images/ccc_other/빠르게 실패하기.webp",
-      "/images/ccc_other/세탁 살림 백과.webp",
-    ];
-
-    setBooks(
-      books.map((book, index) => ({ ...book, src: shuffle(bookImages)[index] }))
-    );
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`/api/aladin/해리포터`);
+        setBooks(response.data.item);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchData();
   }, []);
 
   const handleViewMore = () => {
@@ -66,8 +36,8 @@ const Main_monthbook = () => {
       ) : (
         <div className="book_covers">
           {books.map((book, index) => (
-            <div className="coverbook1" key={index}>
-              <img src={book.src} alt={`book${index + 1}`} />
+            <div className="coverbook1" key={book.isbn}>
+              <img src={book.cover} alt={`book${index + 1}`} />
               <div className="coverexplain">
                 <div className="bookcovertitle">{book.title}</div>
                 <div className="author">{book.author}</div>
